644d095c97e74d0cadf1f91fc25d5667164e866d
gustallafermrd <36135271+gustallafermrd@users.noreply.github.com>
2021-11-16 22:16:02 -0300

    segunda entrega

--- git-ftp-main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "Git Push"
-git push --verbose
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h4>CoderCommerce 2021</h4>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Categorias</li>
+        <li>Nosotros</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
